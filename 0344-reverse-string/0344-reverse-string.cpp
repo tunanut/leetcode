1e377d3f7076e68667caa50419f014115184ea39
tunanut <66516687+tunanut@users.noreply.github.com>
2023-02-22 18:48:37 +0900

    Time: 27 ms (41.65%), Space: 23.2 MB (80.60%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(),s.end());
+        int ss=0,ee=s.size()-1;
+        while(ss<ee)
+            swap(s[ss++],s[ee--]);
     }
 };
