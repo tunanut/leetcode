10c8d9aa1583fc95dfd29e824492d4d53c7981ce
tunanut <66516687+tunanut@users.noreply.github.com>
2023-02-26 19:34:52 +0900

    Time: 297 ms (14.27%), Space: 7.3 MB (56.94%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -4,17 +4,17 @@ public:
         if(s1.length()>s2.length())
             return false;
         int s=0,e=s2.length(),ee=s1.length(),c=0;
-        int m[128]={0,};
-        int n[128]={0,};
+        int m[26]={0,};
+        int n[26]={0,};
         for(int i=0;i<ee;++i)
-            ++m[s1[i]];
+            ++m[s1[i]-'a'];
         for(int i=0;i<e;++i)
         {
             c=0;
             memcpy(n,m,sizeof(m));
             for(int j=i;j<e;++j)
             {
-                if(n[s2[j]]--==0)
+                if(n[s2[j]-'a']--==0)
                     break;
                 if(++c==ee)
                     return true;
