225a0e32612e15dfcbe78b6e9170ad09f5f0c0fc
tunanut <66516687+tunanut@users.noreply.github.com>
2023-02-27 21:25:48 +0900

    Time: 39 ms (75.43%), Space: 32.3 MB (66.65%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -10,38 +10,21 @@
  * };
  */
 class Solution {
-    void check(TreeNode* r1,TreeNode* r2,TreeNode* r3)
+    TreeNode* check(TreeNode* r1,TreeNode* r2)
     {
-        TreeNode* r1l=0,*r1r=0,*r2l=0,*r2r=0;
-        if(r1)
-        {
-            r3->val+=r1->val;
-            r1l=r1->left;
-            r1r=r1->right;
-        }
-        if(r2)
-        {
-            r3->val+=r2->val;
-            r2l=r2->left;
-            r2r=r2->right;
-        }
-        if(r1l||r2l)
-        {
-            r3->left=new TreeNode();
-            check(r1l,r2l,r3->left);
-        }
-        if(r1r||r2r)
-        {
-            r3->right=new TreeNode();
-            check(r1r,r2r,r3->right);
-        }
+        if(r1&&!r2)
+            return r1;
+        if(!r1&&r2)
+            return r2;
+        if(!r1&&!r2)
+            return nullptr;
+        r1->val+=r2->val;
+        r1->left=check(r1->left,r2->left);
+        r1->right=check(r1->right,r2->right);
+        return r1;
     }
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(!root1&&!root2)
-            return 0;
-        TreeNode* r=new TreeNode();
-        check(root1,root2,r);
-        return r;
+        return check(root1,root2);
     }
 };
