55aa00f7573036d19cc1bea3e3415bdd4aa54814
tunanut <66516687+tunanut@users.noreply.github.com>
2023-04-30 18:40:55 +0900

    Time: 3 ms (31.86%), Space: 6.9 MB (16.69%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -10,11 +10,11 @@ public:
         
         for(int r = 1; r < numRows; ++r)
         {
-            auto pr = v[r-1];
+            auto* pr = &v[r-1];
             vector<int> a;
             a.push_back(1);
-            for(int i = 0; i < pr.size() - 1; ++i)
-                a.push_back(pr[i] + pr[i+1]);
+            for(int i = 0; i < pr->size() - 1; ++i)
+                a.push_back((*pr)[i] + (*pr)[i+1]);
             a.push_back(1);
             v.push_back(a);
         }
